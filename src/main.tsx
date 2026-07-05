cc54678804c5f00d35db25d86c3ab1c7bd5c7dd6
Yeongphoto <129649511+Yeongphoto@users.noreply.github.com>
2026-07-06 02:23:55 +0900

    Load asset save guard before app behavior

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './asset-save-guard';
 import './app-behavior';
 import './asset-drag-fix';
 import './touch-sortable';
